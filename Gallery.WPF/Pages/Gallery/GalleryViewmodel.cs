e553cc3a4144f96df76bae19df90e8a16f1a862e
Kim <kim-cv@kim-cv.com>
2019-07-07 14:59:58 +0200

    no message

--- Gallery.WPF/Pages/Gallery/GalleryViewmodel.cs
+++ Gallery.WPF/Pages/Gallery/GalleryViewmodel.cs
@@ -3,11 +3,16 @@ using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Windows.Media.Imaging;
+using System.Windows.Input;
+using System.Windows.Forms;
 
 namespace Gallery.WPF.Views.Gallery
 {
     public class GalleryViewmodel
     {
+        // Events & Commands
+        public ICommand btnCmdChooseImage { get; set; }
+
         public ObservableCollection<BitmapSource> Images { get; set; }
         private readonly IImageRepository imageRepositoryMediator;
 
@@ -18,10 +23,20 @@ namespace Gallery.WPF.Views.Gallery
                 return;
             }
 
+            btnCmdChooseImage = new RelayCommand<BitmapSource>(tmpImage =>
+            {
+                cmdChooseImage(tmpImage);
+            });
+
             imageRepositoryMediator = _imageRepositoryMediator;
             IEnumerable<BitmapSource> tmpImages = imageRepositoryMediator.RetrieveImagesAsThumbs();
 
             Images = new ObservableCollection<BitmapSource>(tmpImages);
+        }
+
+        private void cmdChooseImage(BitmapSource image)
+        {
+            MessageBox.Show("Clicked Image");
         }
     }
 }
--- Gallery.WPF/RelayCommand.cs
+++ Gallery.WPF/RelayCommand.cs
@@ -3,6 +3,27 @@ using System.Windows.Input;
 
 namespace Gallery.WPF
 {
+    public class RelayCommand<T> : ICommand
+    {
+        public event EventHandler CanExecuteChanged;
+        private Action<T> commandTask;
+
+        public RelayCommand(Action<T> _commandTask)
+        {
+            commandTask = _commandTask;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            commandTask((T)parameter);
+        }
+    }
+
     public class RelayCommand : ICommand
     {
         public event EventHandler CanExecuteChanged;
