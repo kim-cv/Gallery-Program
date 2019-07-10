78dd42d48fa5ced6083b668482cbbe3ce1e474bb
Kim <kim-cv@kim-cv.com>
2019-07-10 12:27:23 +0200

    no message

---
+++ Gallery.WPF/Pages/ViewImage/ViewImagePage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows.Controls;
+
+namespace Gallery.WPF.Pages.ViewImage
+{
+    public partial class ViewImagePage : Page
+    {
+        public ViewImagePage(ViewImageViewmodel viewModel)
+        {
+            InitializeComponent();
+            DataContext = viewModel;
+
+            viewModel.OnNavigateToNewPage += NavigateToPage;
+        }
+
+        private void NavigateToPage(AVAILABLE_PAGES pageType, object pageData)
+        {
+            Page page = PageFactory.ConstructPage(pageType, pageData);
+            NavigationService.Navigate(page);
+        }
+    }
+}
---
+++ Gallery.WPF/Pages/ViewImage/ViewImageViewmodel.cs
@@ -0,0 +1,31 @@
+﻿using Gallery.WPF.Interfaces;
+using System.ComponentModel;
+using System.Windows;
+using System.Windows.Media.Imaging;
+
+namespace Gallery.WPF.Pages.ViewImage
+{
+    public class ViewImageViewmodel : IViewmodel, INotifyPropertyChanged
+    {
+        // Events & Commands
+        public event PropertyChangedEventHandler PropertyChanged;
+        public event EventHandlers.NavigateToPageEventHandler OnNavigateToNewPage;
+
+        public BitmapSource image { get; }
+
+        public ViewImageViewmodel(BitmapSource _image)
+        {
+            if (DesignerProperties.GetIsInDesignMode(new DependencyObject()))
+            {
+                return;
+            }
+
+            image = _image;
+        }
+
+        protected void NotifyPropertyChanged(string info)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(info));
+        }
+    }
+}
