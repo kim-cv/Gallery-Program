3ad4189869509f25de65e8f468b56aa31f9db715
Kim <kim-cv@kim-cv.com>
2019-08-12 12:17:18 +0200

    no message

---
+++ Gallery.TestUtils/APIControllerUtils.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Gallery.TestUtils
+{
+    public class APIControllerUtils
+    {
+        public static ControllerContext CreateApiControllerContext(string claimName)
+        {
+            var claims = new List<Claim>()
+            {
+                new Claim(ClaimTypes.Name, claimName)
+            };
+            var identity = new ClaimsIdentity(claims, "TestAuthType");
+            var claimsPrincipal = new ClaimsPrincipal(identity);
+
+            var context = new ControllerContext
+            {
+                HttpContext = new DefaultHttpContext
+                {
+                    User = claimsPrincipal
+                }
+            };
+
+            return context;
+        }
+    }
+}
