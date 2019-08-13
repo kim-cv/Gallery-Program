8e204f44c335144950cb4049734b1d32c618061c
Kim <kim-cv@kim-cv.com>
2019-08-13 10:50:00 +0200

    no message

--- Gallery.API.Test/GalleryControllerTest.cs
+++ Gallery.API.Test/GalleryControllerTest.cs
@@ -1,9 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Security.Claims;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
