1aaf3dc0557c64c6459f2a6ef5a2e1d0b47b4c23
Kim <kim-cv@kim-cv.com>
2019-08-27 12:26:23 +0200

    no message

---
+++ Gallery.API/Helpers/Pagination.cs
@@ -0,0 +1,86 @@
+﻿namespace Gallery.API.Helpers
+{
+    public class Pagination
+    {
+        public int Skip
+        {
+            get
+            {
+                int tmpSkip = PageSize * (PageNumber - 1);
+
+                if (tmpSkip > int.MaxValue)
+                {
+                    return int.MaxValue;
+                }
+                else if (tmpSkip < 0)
+                {
+                    return 0;
+                }
+                else
+                {
+                    return tmpSkip;
+                }
+            }
+        }
+
+        public int Take
+        {
+            get
+            {
+                return PageSize;
+            }
+        }
+
+        public const int maxPageNumber = int.MaxValue;
+        public const int minimumPageNumber = 1;
+        private int _pageNumber = minimumPageNumber;
+        public int PageNumber
+        {
+            get
+            {
+                return _pageNumber;
+            }
+            set
+            {
+                if (value > maxPageNumber)
+                {
+                    _pageNumber = maxPageNumber;
+                }
+                else if (value < minimumPageNumber)
+                {
+                    _pageNumber = minimumPageNumber;
+                }
+                else
+                {
+                    _pageNumber = value;
+                }
+            }
+        }
+
+        public const int maxPageSize = 20;
+        public const int minimumPageSize = 10;
+        private int _pageSize = minimumPageSize;
+        public int PageSize
+        {
+            get
+            {
+                return _pageSize;
+            }
+            set
+            {
+                if (value > maxPageSize)
+                {
+                    _pageSize = maxPageSize;
+                }
+                else if (value < minimumPageSize)
+                {
+                    _pageSize = minimumPageSize;
+                }
+                else
+                {
+                    _pageSize = value;
+                }
+            }
+        }
+    }
+}
