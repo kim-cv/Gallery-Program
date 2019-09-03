97db0b736d1930720ec1c6959df8b7dfb26049f3
Kim <kim-cv@kim-cv.com>
2019-09-03 12:19:48 +0200

    no message

---
+++ Gallery.API/Interfaces/IGalleryService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Gallery.API.Models;
+using Gallery.API.Helpers;
+
+namespace Gallery.API.Interfaces
+{
+    public interface IGalleryService
+    {
+        Task<bool> DoesGalleryExistAsync(Guid galleryId);
+        Task<bool> IsGalleryOwnedByUserAsync(Guid galleryId, Guid userId);
+        Task<GalleryDTO> CreateGalleryAsync(Guid userId, GalleryCreationDTO dto);
+        Task<GalleryDTO> PutGalleryAsync(Guid userId, Guid galleryId, GalleryPutDTO galleryPutDTO);
+        Task<GalleryDTO> GetGalleryAsync(Guid galleryId);
+        Task<IEnumerable<GalleryDTO>> GetGalleriesByUserAsync(Guid userId, Pagination pagination);
+        Task DeleteGalleryAsync(Guid galleryId);
+    }
+}
