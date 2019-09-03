7219cb7096eca9328d79098dbe1a73dceb854205
Kim <kim-cv@kim-cv.com>
2019-09-03 13:59:09 +0200

    no message

--- Gallery.API/Interfaces/IImageService.cs
+++ Gallery.API/Interfaces/IImageService.cs
@@ -1,7 +1,20 @@
-﻿namespace Gallery.API.Interfaces
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Gallery.API.Models;
+using Gallery.API.Helpers;
+
+namespace Gallery.API.Interfaces
 {
     public interface IImageService
     {
+        Task<bool> DoesImageExistAsync(Guid imageId);
+        Task<bool> IsImageInsideGalleryAsync(Guid imageId, Guid galleryId);
+        Task<ImageDTO> CreateImageAsync(Guid userId, Guid galleryId, ImageCreationDTO dto);
+        Task<byte[]> GetImageAsync(Guid imageId, bool thumb, int? thumbWidth, int? thumbHeight, bool? keepAspectRatio);
+        Task<IEnumerable<byte[]>> GetImagesInGalleryAsync(Guid galleryId, Pagination pagination, bool thumb, int? thumbWidth, int? thumbHeight, bool? keepAspectRatio);
+        Task DeleteImageAsync(Guid galleryId, Guid imageId);
+
         byte[] GenerateThumb(byte[] imageData, int maxWidth, int maxHeight, bool keepAspectRatio);
     }
 }
