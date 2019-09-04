da64161fbc7bbd02a7fd5bad2b25c7f5967ed6fa
Kim <kim-cv@kim-cv.com>
2019-09-04 13:02:56 +0200

    no message

---
+++ Gallery.API/Interfaces/IUserService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Threading.Tasks;
+using Gallery.API.Models;
+
+namespace Gallery.API.Interfaces
+{
+    public interface IUserService
+    {
+        Task<bool> DoesUserExist(Guid userId);
+        Task<bool> DoesUserExist(string username);
+        Task<UserDTO> CreateUserAsync(UserCreationDTO dto);
+        Task<UserDTO> GetUserAsync(Guid userId);
+        UserDTO GetUserAsync(string username);
+
+        Task<string> LoginAsync(UserLoginDTO userLoginDTO);
+    }
+}
