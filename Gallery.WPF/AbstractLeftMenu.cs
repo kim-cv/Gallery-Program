e65fbbb47f68180a0f86c2f0972da38e5b4d452b
Kim <kim-cv@kim-cv.com>
2019-09-20 13:42:14 +0200

    no message

---
+++ Gallery.WPF/AbstractLeftMenu.cs
@@ -0,0 +1,44 @@
+﻿using System.Windows.Input;
+using Gallery.BL;
+
+namespace Gallery.WPF
+{
+    public abstract class AbstractLeftMenu : AbstractNavigation
+    {
+        // Commands
+        public ICommand btnCmdChooseGallery { get; set; }
+        public ICommand btnCmdAddGalleryLocation { get; set; }
+        public ICommand btnCmdViewGalleryLocations { get; set; }
+
+        public AbstractLeftMenu()
+        {
+            btnCmdChooseGallery = new RelayCommand<GalleryLocation>(tmpGallery =>
+            {
+                NavigateToChooseGallery(tmpGallery);
+            });
+
+            btnCmdAddGalleryLocation = new RelayCommand(NavigateToAddNewGalleryLocation);
+            btnCmdViewGalleryLocations = new RelayCommand(NavigateToViewGalleryLocations);
+        }
+
+        private void NavigateToAddNewGalleryLocation()
+        {
+            NavigateToPage(AVAILABLE_PAGES.AddGalleryLocation, null);
+        }
+
+        private void NavigateToViewGalleryLocations()
+        {
+            NavigateToPage(AVAILABLE_PAGES.GalleryLocations, null);
+        }
+
+        private void NavigateToChooseGallery(GalleryLocation gallery)
+        {
+            if (gallery == null)
+            {
+                return;
+            }
+
+            NavigateToPage(AVAILABLE_PAGES.Gallery, gallery);
+        }
+    }
+}
