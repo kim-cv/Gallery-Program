6390493100e037d1ab2bd02bed9eb1362ca1e092
Kim <kim-cv@kim-cv.com>
2019-09-20 13:47:53 +0200

    no message

--- Gallery.WPF/Pages/GalleryLocations/GalleryLocationsViewmodel.cs
+++ Gallery.WPF/Pages/GalleryLocations/GalleryLocationsViewmodel.cs
@@ -2,17 +2,11 @@
 using Gallery.WPF.Interfaces;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
-using System.Windows.Input;
 
 namespace Gallery.WPF.Pages.GalleryLocations
 {
-    public class GalleryLocationsViewmodel : IViewmodel
+    public class GalleryLocationsViewmodel : AbstractLeftMenu, IViewmodel
     {
-        // Events & Commands
-        public ICommand btnCmdChooseGallery { get; set; }
-        public ICommand btnCmdAddGalleryLocation { get; set; }
-        public event EventHandlers.NavigateToPageEventHandler OnNavigateToNewPage;
-
         public ObservableCollection<GalleryLocation> GalleryLocations
         { get; set; }
         private readonly GalleryDataSQLiteRepository galleryDataSQLiteRepository;
@@ -24,30 +18,8 @@ namespace Gallery.WPF.Pages.GalleryLocations
                 return;
             }
 
-            btnCmdChooseGallery = new RelayCommand<GalleryLocation>(tmpGallery =>
-            {
-                cmdChooseGallery(tmpGallery);
-            });
-
             galleryDataSQLiteRepository = _galleryDataSQLiteRepository;
             GalleryLocations = new ObservableCollection<GalleryLocation>(galleryDataSQLiteRepository.RetrieveGalleryLocations());
-
-            btnCmdAddGalleryLocation = new RelayCommand(NavigateToAddNewGalleryLocation);
-        }
-
-        private void NavigateToAddNewGalleryLocation()
-        {
-            OnNavigateToNewPage?.Invoke(AVAILABLE_PAGES.AddGalleryLocation, null);
-        }
-
-        private void cmdChooseGallery(GalleryLocation gallery)
-        {
-            if (gallery == null)
-            {
-                return;
-            }
-
-            OnNavigateToNewPage?.Invoke(AVAILABLE_PAGES.Gallery, gallery);
         }
     }
 }
