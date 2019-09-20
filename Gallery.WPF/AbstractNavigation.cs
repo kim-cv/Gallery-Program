979762b45e9505c13ed911c7abc998090696f49e
Kim <kim-cv@kim-cv.com>
2019-09-20 13:40:05 +0200

    no message

---
+++ Gallery.WPF/AbstractNavigation.cs
@@ -0,0 +1,13 @@
+﻿namespace Gallery.WPF
+{
+    public abstract class AbstractNavigation
+    {
+        // Events
+        public event EventHandlers.NavigateToPageEventHandler OnNavigateToNewPage;
+
+        public void NavigateToPage(AVAILABLE_PAGES page, object data)
+        {
+            OnNavigateToNewPage?.Invoke(page, data);
+        }
+    }
+}
