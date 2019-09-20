23d74a3809d38488f5fe497ffbc5401f4a201f2a
Kim <kim-cv@kim-cv.com>
2019-09-20 14:22:26 +0200

    no message

--- Gallery.WPF/AbstractLeftMenu.cs
+++ Gallery.WPF/AbstractLeftMenu.cs
@@ -6,17 +6,11 @@ namespace Gallery.WPF
     public abstract class AbstractLeftMenu : AbstractNavigation
     {
         // Commands
-        public ICommand btnCmdChooseGallery { get; set; }
         public ICommand btnCmdAddGalleryLocation { get; set; }
         public ICommand btnCmdViewGalleryLocations { get; set; }
 
         public AbstractLeftMenu()
         {
-            btnCmdChooseGallery = new RelayCommand<GalleryLocation>(tmpGallery =>
-            {
-                NavigateToChooseGallery(tmpGallery);
-            });
-
             btnCmdAddGalleryLocation = new RelayCommand(NavigateToAddNewGalleryLocation);
             btnCmdViewGalleryLocations = new RelayCommand(NavigateToViewGalleryLocations);
         }
@@ -29,16 +23,6 @@ namespace Gallery.WPF
         private void NavigateToViewGalleryLocations()
         {
             NavigateToPage(AVAILABLE_PAGES.GalleryLocations, null);
-        }
-
-        private void NavigateToChooseGallery(GalleryLocation gallery)
-        {
-            if (gallery == null)
-            {
-                return;
-            }
-
-            NavigateToPage(AVAILABLE_PAGES.Gallery, gallery);
         }
     }
 }
--- Gallery.WPF/Pages/GalleryLocations/GalleryLocationsViewmodel.cs
+++ Gallery.WPF/Pages/GalleryLocations/GalleryLocationsViewmodel.cs
@@ -1,12 +1,16 @@
-﻿using Gallery.BL;
-using Gallery.WPF.Interfaces;
-using System.Collections.ObjectModel;
+﻿using System.Collections.ObjectModel;
 using System.ComponentModel;
+using System.Windows.Input;
+using Gallery.BL;
+using Gallery.WPF.Interfaces;
 
 namespace Gallery.WPF.Pages.GalleryLocations
 {
     public class GalleryLocationsViewmodel : AbstractLeftMenu, IViewmodel
     {
+        // Commands
+        public ICommand btnCmdChooseGallery { get; set; }
+
         public ObservableCollection<GalleryLocation> GalleryLocations
         { get; set; }
         private readonly GalleryDataSQLiteRepository galleryDataSQLiteRepository;
@@ -20,6 +24,15 @@ namespace Gallery.WPF.Pages.GalleryLocations
 
             galleryDataSQLiteRepository = _galleryDataSQLiteRepository;
             GalleryLocations = new ObservableCollection<GalleryLocation>(galleryDataSQLiteRepository.RetrieveGalleryLocations());
+
+            btnCmdChooseGallery = new RelayCommand<GalleryLocation>(tmpGallery =>
+            {
+                if (tmpGallery == null)
+                {
+                    return;
+                }
+                NavigateToPage(AVAILABLE_PAGES.Gallery, tmpGallery);
+            });
         }
     }
 }
