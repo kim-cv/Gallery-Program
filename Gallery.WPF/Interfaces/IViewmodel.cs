18a471b60c6c2306f6159782528dce82366f4f08
Kim <kim-cv@kim-cv.com>
2019-09-25 13:28:18 +0200

    no message

--- Gallery.WPF/Interfaces/IViewmodel.cs
+++ Gallery.WPF/Interfaces/IViewmodel.cs
@@ -1,9 +1,14 @@
-﻿using Gallery.WPF.EventHandlers;
+﻿using System.Windows;
+using Gallery.WPF.EventHandlers;
 
 namespace Gallery.WPF.Interfaces
 {
     public interface IViewmodel
     {
         event NavigateToPageEventHandler OnNavigateToNewPage;
+        public void NavigationErrorPopup(string errorMessage)
+        {
+            MessageBox.Show(errorMessage, "Navigation Error", MessageBoxButton.OK, MessageBoxImage.Warning);
+        }
     }
 }
